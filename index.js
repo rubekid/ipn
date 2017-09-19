17e68bb568cd975d6317d681f17978e5d8967609
杨立哲 <562618993@qq.com>
2017-09-19 18:37:51 +0800

    1、API_BASH_PATH => API_BASE_PATH

---
+++ index.js
@@ -0,0 +1,22 @@
+import './lib/common/common'
+
+import AuthEncrypt from './lib/auth/auth-encrypt'
+import TokenManager from './lib/auth/token-manager'
+
+import Http from './lib/http/http'
+
+import AjaxCache from './lib/cache/ajax-cache'
+import Storage from './lib/cache/storage'
+
+import DateUtil from './lib/utils/date-util'
+import Map from './lib/utils/map'
+
+export {
+  AuthEncrypt,
+  TokenManager,
+  Http,
+  AjaxCache,
+  Storage,
+  DateUtil,
+  Map
+}
